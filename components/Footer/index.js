f9440af9475a7551a574208261dbd7bd07fb594f
Solomon Sunmola <sunmolasolomon@gmail.com>
2023-02-01 07:44:58 +0100

    changes

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -3,27 +3,28 @@ import Socials from "../Socials";
 // import Link from "next/link";
 import Button from "../Button";
 
-const Footer = ({}) => {
+const Footer = ({ }) => {
   return (
     <>
-      <div className="mt-5 laptop:mt-40 p-2 laptop:p-0">
+      <div className="mt-5 laptop:mt-40 p-2 laptop:p-0 grid place-items-center h-screen">
         <div>
-          <h1 className="text-2xl text-bold">Contact</h1>
+          <h1 className="text-5xl text-center text-bold">Contact</h1>
           <div className="mt-10">
             <h1 className="text-3xl tablet:text-6xl laptop:text-6xl laptopl:text-8xl text-bold">
-              LET&apos;S WORK
+              LET&apos;S WORK TOGETHER
             </h1>
-            <h1 className="text-3xl tablet:text-6xl laptop:text-6xl laptopl:text-8xl text-bold">
-              TOGETHER
-            </h1>
-            <Button type="primary">Schedule a call</Button>
-            <div className="mt-10">
-              <Socials />
+            <div className="grid place-items-center">
+              <Button type="primary" key={'mail'} onClick={() => window.open('mailto:sunmolasolomon@gmail.com')}>Schedule a call</Button>
             </div>
+
+            
           </div>
         </div>
       </div>
-     
+      <div className="mt-10 ml-5">
+              <Socials />
+      </div>
+
     </>
   );
 };
--- components/Socials/index.js
+++ components/Socials/index.js
@@ -7,7 +7,7 @@ const Socials = ({ className }) => {
   return (
     <div className={`${className} flex flex-wrap mob:flex-nowrap link`}>
       {yourData.socials.map((social, index) => (
-        <Button key={index} onClick={() => window.open(social.link)}>
+        <Button type="primary" key={index} onClick={() => window.open(social.link)}>
           {social.title}
         </Button>
       ))}
